0f2b16944a7c38b57a087166223439450d71b641
LEI <guillaume.frichet@gmail.com>
2018-08-29 11:35:25 +0200

    un

--- brew-unbundle.sh
+++ brew-unbundle.sh
@@ -41,15 +41,17 @@ while read -r line; do
   name="${line#$word }"
   case "$word" in
     # "") echo >&2 "$file: empty line"; exit 1 ;;
-    brew) cmd=$word uninstall $name ;;
-    cask) cmd=brew $word uninstall $name ;;
-    tap) cmd=brew untap $name ;;
+    brew) cmd="$word uninstall $name" ;;
+    cask) cmd="brew $word uninstall $name" ;;
+    tap) cmd="brew untap $name" ;;
     # TODO: handle custom appdir
-    mas) cmd=trash /Applications/$name.app ;;
+    mas) cmd="trash /Applications/$name.app" ;;
     *) continue ;;
   esac
+  # Print the command in verbose mode
   if [ "$DOT_VERBOSE" = 1 ]; then
     echo "$cmd"
   fi
-  $cmd
+  # Execute the command, allowing quoted arguments
+  eval "$cmd"
 done <"$file"
